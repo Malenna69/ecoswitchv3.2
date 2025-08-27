4b3b5a4f817f8b681e482e7546a5c61d846900a1
Chafique <zmalenna20@gmail.com>
2025-08-27 18:32:44 +0200

    Delete ecoswitch_lite_v3_2 directory

--- ecoswitch_lite_v3_2/client_data.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import json
-
-def load_client(path_json: str):
-    with open(path_json, "r", encoding="utf-8") as f:
-        return json.load(f)
--- ecoswitch_lite_v3_2/streamlit_app.py
+++
@@ -1,3 +0,0 @@
-# Entry point for hosted platforms
-# Just import the UI app; it renders on import.
-import ui.app_streamlit  # noqa
